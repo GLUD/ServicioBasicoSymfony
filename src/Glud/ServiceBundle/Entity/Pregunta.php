64893b414b14e2abfd9834720d8d2cea960e687e
Steven Sierra <bizoru@gmail.com>
2016-05-17 23:45:41 -0500

    Crud de examenes

---
+++ src/Glud/ServiceBundle/Entity/Pregunta.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Glud\ServiceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Pregunta
+ *
+ * @ORM\Table(name="pregunta")
+ * @ORM\Entity(repositoryClass="Glud\ServiceBundle\Repository\PreguntaRepository")
+ */
+class Pregunta
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="texto", type="string", length=255, nullable=true)
+     */
+    private $texto;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set texto
+     *
+     * @param string $texto
+     * @return Pregunta
+     */
+    public function setTexto($texto)
+    {
+        $this->texto = $texto;
+
+        return $this;
+    }
+
+    /**
+     * Get texto
+     *
+     * @return string 
+     */
+    public function getTexto()
+    {
+        return $this->texto;
+    }
+}
